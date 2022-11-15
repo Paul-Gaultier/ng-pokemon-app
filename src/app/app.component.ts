5c278724f4c629700127f24d9239f1c6a764a698
Paul78330 <paul.gaultier76@yahoo.com>
2022-11-15 15:36:46 +0100

    aap-component-cycle-de-vie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; //importation de notre interface OnInit
 
 @Component({
-  selector: 'app-root',//selector permet de donner un nom à notre compoant
+  selector: 'app-root',
+
+
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <h1>{{title}}</h1>
+    <h1>{{pokemonList[1]}}</h1>
   `,
   styles: []
 })
-export class AppComponent {
-  title = 'Hello World';
+export class AppComponent implements OnInit{//implementation de notre interface au niveau de notre composant
+
+  //propriété pokemon
+  pokemonList = ['Bulbizarre','Salamèche','Carapuce'];
+
+    ngOnInit(): void {//def de la méthode associée à notre nouvelle interface (Oninit)
+  
+      console.table(this.pokemonList);//Une table de Pokemons dans notre console
+      //this. nous permet d'acceder à la propriété de notre classe,c-a-d PokemonList autrement Angular ne sais pas de quoi il s'agit
+  }
 }
