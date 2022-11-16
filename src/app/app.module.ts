45dd01d420876afcd1a55a85536bcf1e600771d2
Paul78330 <paul.gaultier76@yahoo.com>
2022-11-16 15:23:37 +0100

    pokemon_module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonModule } from './pokemon/pokemon.module'
 
 @NgModule({
-  declarations: [
+  declarations: [//liste de tous les composants, les directives, les pipes appartenant au module
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
     PageNotFoundComponent
   ],
-  imports: [
+  imports: [//déclaration de tous les elemnts (venues d'autres fichiers) dont on a besoin dans notre module
     BrowserModule,
-    AppRoutingModule
+    PokemonModule,//Nous relions ainsi notre module racine à son module fils pokemonModule
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//permet d'utiliser le système d'injection de dépendances d'Angular
+  bootstrap: [AppComponent]//le compossant affiché sera celui entre crochet (ici le composant racine)
 })
 export class AppModule { }
---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import {RouterModule, Routes} from '@angular/router'
+
+const pokemonRoutes: Routes = [
+  {path: 'pokemons', component: ListPokemonComponent},
+  {path: 'pokemon/:id', component: DetailPokemonComponent},
+]
+
+
+
+@NgModule({
+  declarations: [
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+    ListPokemonComponent,
+    DetailPokemonComponent,
+  ],
+  imports: [
+    CommonModule,
+
+    
+    RouterModule.forChild(pokemonRoutes)
+    /**Je vais utiliser le RouterModule fourni par Angular et 
+     * je lui ajoute des routes grâce à l'option .forChild
+     * et a l'intérieur je passe mes routes pokemonRoutes
+     */
+  ]
+})
+export class PokemonModule { }
